2cb52b107ddfbc88eb20230f5c667f49d024a59e
Oleg Chuchko <motoblock.e@gmail.com>
2019-03-03 20:54:24 +0000

    create function Fibonachi

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function fib(n){
-if (n==1||n==2) return 1;
-else return fib(n-2)+fib(n-1);		
+// function fib(n){
+// if (n==1||n==2) return 1;
+// else return fib(n-2)+fib(n-1);		
+// }
+// const userNum= parseFloat(prompt("Enter your order number of Fibonacci number"));
+// alert(fib(userNum));
+
+
+
+function fibNum(n) {
+  return n <= 1 ? n : fibNum(n - 1) + fibNum(n - 2);
 }
+
 const userNum= parseFloat(prompt("Enter your order number of Fibonacci number"));
-alert(fib(userNum));
+
+alert(fibNum(userNum))
